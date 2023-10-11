cd01dae8b7f3da85e635a0be29d5455d0ce29091
Giada Unger <giada.unger02@gmail.com>
2023-10-11 14:07:02 +0200

    Added a second animal

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ class Animal:
         self.name = name
 
     def what_kind_of_animal(self):
-        print(f"Hello! My name is {self.name} and I'm a {self.animal}, my breed is {self.breed}")
+        print(f"Hello! My name is {self.name} and I'm a {self.animal}, my breed is {self.breed}.")
     
 
-animal_1 = Animal("Dog", "chihuahua", "Pewee")
+animal_1 = Animal("Dog", "Chihuahua", "Pewee")
+animal_2 = Animal("Cat", "Ragdoll", "Bell")
+
 animal_1.what_kind_of_animal()
+animal_2.what_kind_of_animal()

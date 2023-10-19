57ce9a1ac23a30fd996033e01910de4372172a03
Giada Unger <giada.unger02@gmail.com>
2023-10-19 15:03:04 +0200

    Added som subclasses

--- animal_class.py
+++ animal_class.py
@@ -7,3 +7,22 @@ class Animal:
 
     def sleep(self):
         print("This animal is sleeping")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("This fish is swimming")
+
+class Bird(Animal):
+    def fly(self):
+        print("This bird is flying")
+
+class Rabbit(Animal):
+    def run(self):
+        print("This rabbit is running")
+
+
+
+fish = Fish()
+bird = Bird()
+rabbit = Rabbit()

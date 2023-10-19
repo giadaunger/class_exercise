2b3b3576d50199b5cb138c3470f80b4340ef1f33
Giada Unger <giada.unger02@gmail.com>
2023-10-19 14:05:40 +0200

    Accessed the car class

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from car_class import Car
 
+car1 = Car("Chevy", "Corvette", 2021, "red")
+
+print("Car:")
+print(car1.make)
+print(car1.model)
+print(car1.year)
+print(car1.color)
+
+car1.drive()
+car1.stop()

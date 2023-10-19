7638e6034f1c1426412b2047bc8cfc5fecee2e7d
Giada Unger <giada.unger02@gmail.com>
2023-10-19 14:49:38 +0200

    Redid the animal class

--- animal_class.py
+++ animal_class.py
@@ -1,24 +1,9 @@
 class Animal:
-    def __init__(self, animal, name):
-        self.animal = animal
-        self.name = name
-
-    def what_kind_of_animal(self):
-        print(f"Hello! My name is {self.name} and I'm a {self.animal}.")
-
-
-class Breed(Animal):
-    def __init__(self, animal, name, breed, length, weight):
-        self.breed = breed
-        self.length = length
-        self.weight = weight
-
-        Animal.__init__(self, animal, name)
     
+    alive = True
 
-animal_1 = Breed("Dog", "Pewee", "Chihuahua", 25, 1.7)
-#animal_2 = Animal("Cat", "Bell")
+    def eat(self):
+        print("This animal is eating")
 
-animal_1.what_kind_of_animal()
-print(animal_1)
-#animal_2.what_kind_of_animal()
+    def sleep(self):
+        print("This animal is sleeping")

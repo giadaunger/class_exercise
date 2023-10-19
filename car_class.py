ca0e3ad28b3cf90392422f4911e342fa20a643f2
Giada Unger <giada.unger02@gmail.com>
2023-10-19 13:52:58 +0200

    Added some functions

--- car_class.py
+++ car_class.py
@@ -3,3 +3,9 @@ class Car:
     model = None
     year = None
     color = None
+
+    def drive(self):
+        print("This car is driving")
+
+    def stop(self):
+        print("This car has stopped")

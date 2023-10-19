fb81081ef29bcc8cf1c9ccca34187594fca725f3
Giada Unger <giada.unger02@gmail.com>
2023-10-19 13:40:57 +0200

    Added a subclass

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 class Animal:
-    def __init__(self, animal, breed, name):
+    def __init__(self, animal, name):
         self.animal = animal
-        self.breed = breed
         self.name = name
 
     def what_kind_of_animal(self):
-        print(f"Hello! My name is {self.name} and I'm a {self.animal}, my breed is {self.breed}.")
+        print(f"Hello! My name is {self.name} and I'm a {self.animal}.")
+
+
+class Breed(Animal):
+    def __init__(self, animal, name, breed, length, weight):
+        self.breed = breed
+        self.length = length
+        self.weight = weight
+
+        Animal.__init__(self, animal, name)
     
 
-animal_1 = Animal("Dog", "Chihuahua", "Pewee")
-animal_2 = Animal("Cat", "Ragdoll", "Bell")
+animal_1 = Breed("Dog", "Pewee", "Chihuahua", 25, 1.7)
+#animal_2 = Animal("Cat", "Bell")
 
 animal_1.what_kind_of_animal()
-animal_2.what_kind_of_animal()
+print(animal_1)
+#animal_2.what_kind_of_animal()
